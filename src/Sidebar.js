e0d28fcb75a1ff6c0768cc938e16a9f4bde25390
fardin <firdozking@gmail.com>
2024-04-07 09:31:22 +0530

    last Push

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,97 +1,104 @@
-import React from 'react'
-import './Sidebar.css'
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-import AddIcon from '@material-ui/icons/Add'
-import SidebarChannel from './SidebarChannel'
-import SignalCellularAltIcon from '@material-ui/icons/SignalCellularAlt'
-import InfoOutlinedIcon from '@material-ui/icons/InfoOutlined'
-import CallIcon from '@material-ui/icons/Call'
-import { Avatar } from '@material-ui/core'
-import MicIcon from '@material-ui/icons/Mic'
-import HeadsetIcon from '@material-ui/icons/Headset'
-import SettingsIcon from '@material-ui/icons/Settings'
-import { useSelector } from 'react-redux'
-import { selectUser } from './features/userSlice'
-import db, { auth } from './firebase'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React from "react";
+import "./Sidebar.css";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import AddIcon from "@material-ui/icons/Add";
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import db, { auth } from "./firebase";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const Sidebar = () => {
-    const user = useSelector(selectUser)
-    const [channels, setChannels] = useState([])
+  const user = useSelector(selectUser);
+  const [channels, setChannels] = useState([]);
 
-    useEffect(() => {
-        db.collection('channels').onSnapshot(snapshot => {
-            setChannels(snapshot.docs.map(doc => ({
-                id: doc.id,
-                channel: doc.data()
-            })))
-        })
-    }, [])
+  useEffect(() => {
+    db.collection("channels").onSnapshot((snapshot) => {
+      setChannels(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          channel: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
-    const handleAddChannel = (e) => {
-        e.preventDefault()
+  const handleAddChannel = (e) => {
+    e.preventDefault();
 
-        const channelName = prompt('Enter a new channel name')
+    const channelName = prompt("Enter a new channel name");
 
-        if (channelName) {
-            db.collection('channels').add({
-                channelName: channelName
-            })
-
-        }
+    if (channelName) {
+      db.collection("channels").add({
+        channelName: channelName,
+      });
     }
+  };
 
-    return (
-        <div className='sidebar' >
-            <div className="sidebar__top">
-                <h3>Learn-It!</h3>
-                <ExpandMoreIcon />
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Learn-It!</h3>
+        <ExpandMoreIcon />
+      </div>
 
-            <div className="sidebar__channels">
-                <div className="sidebar__channelsHeader">
-                    <div className="sidebar__header">
-                        <ExpandMoreIcon />
-                        <h4>Subject Community</h4>
-                    </div>
+      <div className="sidebar__channels">
+        <div className="sidebar__channelsHeader">
+          <div className="sidebar__header">
+            <ExpandMoreIcon />
+            <h4>Subject Community</h4>
+          </div>
 
-                    <AddIcon onClick={handleAddChannel} className='sidebar__addChannel' />
-                </div>
-                <div className="sidebar__channelsList">
-                    {
-                        channels.map(({ id, channel }) => (
-                            <SidebarChannel key={id} id={id} channelName={channel.channelName} />
-                        ))
-                    }
-                </div>
-            </div>
-
-            <div className="sidebar__voice">
-                <SignalCellularAltIcon className='sidebar__voiceIcons' fontSize='large' />
-                <div className="sidebar__voiceInfo">
-                    <a href="http://localhost:3000/" target='_blank'>Aaja bhai</a>
-                    <h3>Video Call</h3>
-                    <p>Stream</p>
-                </div>
-
-                <div className="sidebar__voiceIcons">
-                    <InfoOutlinedIcon />
-                    <CallIcon />
-                </div>
-            </div>
-            <div className="sidebar__profile">
-                <div className="sidebar__profileInfo">
-                    <h3>{user.displayName}</h3>
-                    <p>#{user.uid.substring(0, 5)}</p>
-                </div>
-
-                <div className="sidebar__profileIcons">
-                <Avatar src={user.photo} onClick={() => auth.signOut()} />
-                </div>
-            </div>
+          <AddIcon onClick={handleAddChannel} className="sidebar__addChannel" />
         </div>
-    )
-}
+        <div className="sidebar__channelsList">
+          {channels.map(({ id, channel }) => (
+            <SidebarChannel
+              key={id}
+              id={id}
+              channelName={channel.channelName}
+            />
+          ))}
+        </div>
+      </div>
 
-export default Sidebar
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcons"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <a href="http://localhost:3000/" target="_blank">
+            <h3>Video Call</h3>
+            <p>Stream</p>
+          </a>
+        </div>
+
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
+        </div>
+      </div>
+      <div className="sidebar__profile">
+        <div className="sidebar__profileInfo">
+          <h3>{user.displayName}</h3>
+          <p>#{user.uid.substring(0, 5)}</p>
+        </div>
+
+        <div className="sidebar__profileIcons">
+          <Avatar src={user.photo} onClick={() => auth.signOut()} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
